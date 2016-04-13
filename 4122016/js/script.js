e79c9b7f25f5949f16f3bdd19f1ec5344ef988b7
Alex Kelly <ajkelly@gmail.com>
2016-04-12 20:18:46 -0500

    validation email
    Added email validation through javascript

--- 4122016/js/script.js
+++ 4122016/js/script.js
@@ -3,6 +3,13 @@ $(function()
 
 $("#button-blue").click(function(e) {
     var url = "../contactform.php"; // the script where you handle the form input.
+    var email=$("#email").val();
+    if (!validateEmail(email)){
+    	
+       	    $("#contact-form").append("<p>Please enter a valid email bitch.</p>");
+
+    	return false;
+    }
     var data=$("#contact-form").serializeArray();
     $.ajax({
        type: "POST",
@@ -25,3 +32,9 @@ $("#button-blue").click(function(e) {
 
 
 });
+
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
